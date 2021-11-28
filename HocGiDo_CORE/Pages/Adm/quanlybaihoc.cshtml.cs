18b7550f497707bc8f245053d7364216d3be36a1
TaeiTaei <nguyenphatthanhtai@mail.com>
2021-11-28 18:58:58 +0700

    Update fix some bug

--- HocGiDo_CORE/Pages/Adm/quanlybaihoc.cshtml.cs
+++ HocGiDo_CORE/Pages/Adm/quanlybaihoc.cshtml.cs
@@ -16,27 +16,24 @@ namespace HocGiDo_CORE.Pages.Adm
         public Course listCourse { get; set; }
         public async Task<IActionResult> OnGet()
         {
-            //var logined = HttpContext.Session.GetString("AdminLogined");
-            //if (logined != null)
-            //{
-            //    UserInf userInf = JsonConvert.DeserializeObject<UserInf>(logined);
-            //    if (userInf.user.Quyen == true)
-            //    {
-            //        listCourse = await new ExcuteJsonClass().getCourse();
-            //        return Page();
-            //    }
-            //    else
-            //    {
-            //        return RedirectToPage("/Adm/LoginAdmin");
-            //    }
-            //}
-            //else
-            //{
-            //    return RedirectToPage("/Adm/LoginAdmin");
-            //}
-
-            listCourse = await new ExcuteJsonClass().getCourse();
-            return Page();
+            var logined = HttpContext.Session.GetString("AdminLogined");
+            if (logined != null)
+            {
+                UserInf userInf = JsonConvert.DeserializeObject<UserInf>(logined);
+                if (userInf.user.Quyen == true)
+                {
+                    listCourse = await new ExcuteJsonClass().getCourse();
+                    return Page();
+                }
+                else
+                {
+                    return RedirectToPage("/Adm/LoginAdmin");
+                }
+            }
+            else
+            {
+                return RedirectToPage("/Adm/LoginAdmin");
+            }
         }
     }
 }
