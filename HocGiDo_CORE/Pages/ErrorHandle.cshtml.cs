26dc82170e8959cc8177531ec02f3a9570f2d99d
TaeiTaei <nguyenphatthanhtai@mail.com>
2021-12-17 15:11:57 +0700

    Update default error page

---
+++ HocGiDo_CORE/Pages/ErrorHandle.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HocGiDo_CORE.Pages
+{
+    public class ErrorHandleModel : PageModel
+    {
+        public ActionResult OnGet()
+        {
+            return Page();
+        }
+    }
+}
--- HocGiDo_CORE/Startup.cs
+++ HocGiDo_CORE/Startup.cs
@@ -48,9 +48,9 @@ namespace HocGiDo_CORE
             }
             else
             {
+                //app.UseStatusCodePages();
+                app.UseStatusCodePagesWithRedirects("/ErrorHandle");
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
             app.UseSession();
